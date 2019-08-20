af98220b54df325f9be70f32da633d43a81b06e7
root <sebastian@analitic.cl>
2019-08-19 22:02:20 -0400

    crear componente auth

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
-
+ 
 @NgModule({
   imports: [
     CommonModule,
